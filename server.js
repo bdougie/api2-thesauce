59967d6ecec81789b45279aea3b266d27ce1362c
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2021-10-22 15:24:44 -0700

    working fastify

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // Require the framework and instantiate it
 import { createClient } from '@supabase/supabase-js'
-const SUPABASE_URL = "https://ibcwmlhcimymasokhgvn.supabase.co"
+const SUPABASE_URL = "https://ibcwmlhcimymasokhgvn.supabase.co/"  
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyOTkzMDc3OCwiZXhwIjoxOTQ1NTA2Nzc4fQ.zcdbd7kDhk7iNSMo8SjsTaXi0wlLNNQcSZkzZ84NUDg"
-const supabase = createClient([SUPABASE_URL], [SUPABASE_ANON_KEY])
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
 // Make a request
 let { data: recommendations, error } = await supabase
@@ -10,12 +10,14 @@ let { data: recommendations, error } = await supabase
   .select('*')
 
 
-console.log(recommendations)
-const fastify = require('fastify')({ logger: true })
-
+// console.log(recommendations)
+import Fastify from 'fastify'
+const fastify = Fastify({
+  logger: true
+})
 // Declare a route
 fastify.get('/', async (request, reply) => {
-  return { hello: 'world' }
+  return { recommendations }
 })
 
 // Run the server!
